5a24f6e2dec41af2b33d71b1aceeb2257db7b652
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-28 21:10:11 +0200

    Added Main method

--- Birds/Program.cs
+++ Birds/Program.cs
@@ -6,7 +6,19 @@
         {
             while (true)
             {
-                Bird bird
+                Bird bird;
+                Console.Write("\nWciśnij G (gołąb) lub S (struś): ");
+                char key = Char.ToUpper(Console.ReadKey().KeyChar);
+                if (key == 'G') bird = new Pigeon();
+                else if (key == 'S') bird = new Ostrich();
+                else return;
+                Console.Write("\nIle jaj składa ptak? ");
+                if (!int.TryParse(Console.ReadLine(), out int numberOfEggs)) return;
+                Egg[] eggs = bird.LayEggs(numberOfEggs);
+                foreach (Egg egg in eggs)
+                {
+                    Console.Write(egg.Description);
+                }
             }
         }
     }
