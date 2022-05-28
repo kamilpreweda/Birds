2036241d215575f171667221b11e0677e87fcfeb
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-28 21:06:11 +0200

    Dodaj pliki projektów.

---
+++ Birds/Bird.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birds
+{
+    public class Bird
+    {
+        public static Random Randomizer = new Random();
+        public virtual Egg[] LayEggs(int numberOfEggs)
+        {
+            Console.Error.WriteLine("Metoda Bird.LayEggs nie powinna być wywoływana");
+        }
+    }
+}
---
+++ Birds/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Birds
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Bird bird
+            }
+        }
+    }
+}
