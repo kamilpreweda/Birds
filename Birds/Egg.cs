fe7a0f9f0eb1c665b8d2b898c40d44592e42ccce
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-28 21:12:59 +0200

    Added Egg class

---
+++ Birds/Egg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birds
+{
+    public class Egg
+    {
+        public double Size { get; private set; }
+        public string Color { get; private set; }
+        public Egg(double size, string color)
+        {
+            Size = size;
+            Color = color;
+        }
+        public string Description
+        {
+            get { return $"{Color} jajo o wielkości {Size:0.0} cm."; }
+        }
+    }
+}
