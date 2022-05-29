cabe338a18637aa696976bdbec5ae55b201ceb70
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-29 19:03:30 +0200

    Added BrokenEgg class

---
+++ Birds/BrokenEgg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birds
+{
+    public class BrokenEgg : Egg
+    {
+        public BrokenEgg(double size, string color) : base(size, color)
+        {
+            Console.WriteLine("Ptak złożył pęknięte jajo");
+        }
+    }
+}
